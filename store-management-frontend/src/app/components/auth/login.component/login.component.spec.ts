24924eddee28894cdff229732978e66a62e4bdd5
MrSachin-05 <sachidanandapanigrahi96@gmail.com>
2026-03-24 11:50:21 +0530

    login component specs updaed

--- store-management-frontend/src/app/components/auth/login.component/login.component.spec.ts
+++ store-management-frontend/src/app/components/auth/login.component/login.component.spec.ts
@@ -1,25 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
-  let fixture: ComponentFixture<LoginComponent>;
   let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent],
-    }).compileComponents();
-  });
+      imports: [LoginComponent]
+    })
+    .compileComponents();
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  describe('Initialization', () => {
-    it('should create the component', () => {
-      expect(component).toBeTruthy();
-    });
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 });
